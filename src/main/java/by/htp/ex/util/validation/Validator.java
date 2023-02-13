e1bb1ea09483cf007121b628a4fdb40a249ccf86
TatsianaMalashankava <109862123+TanyaM3@users.noreply.github.com>
2023-02-13 10:32:28 +0100

    Update Validator.java

--- src/main/java/by/htp/ex/util/validation/Validator.java
+++ src/main/java/by/htp/ex/util/validation/Validator.java
@@ -3,4 +3,5 @@ package by.htp.ex.util.validation;
 public interface Validator {
 	String LOGIN_TEMPLATE_REGEX = "\\w{4,16}";
 	String PASSWORD_TEMPLATE_REGEX = "\\w{4,8}";
+	String EMAIL_TEMPLATE_REGEX = "([.[^@\\\\s]]+)@([.[^@\\\\s]]+)\\\\.([a-z]+)";
 }
