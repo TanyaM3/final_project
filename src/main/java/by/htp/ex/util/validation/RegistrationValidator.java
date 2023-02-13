6d587dfcbfbe0eae658a041614675aa91d3e9af8
TatsianaMalashankava <109862123+TanyaM3@users.noreply.github.com>
2023-02-13 10:32:44 +0100

    Update RegistrationValidator.java

--- src/main/java/by/htp/ex/util/validation/RegistrationValidator.java
+++ src/main/java/by/htp/ex/util/validation/RegistrationValidator.java
@@ -1,7 +1,5 @@
 package by.htp.ex.util.validation;
 
 public interface RegistrationValidator extends Validator {
-	String EMAIL_TEMPLATE_REGEX = "([.[^@\\\\s]]+)@([.[^@\\\\s]]+)\\\\.([a-z]+)";
-
 	boolean checkRegistrationData(String login, String password, String email);
 }
